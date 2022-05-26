8a8856161bf9638948f2c65c0656707da44902fd
mrcreasey <mrcreasey@gmail.com>
2022-05-26 20:01:33 +0200

    add EDA - missing values and categorical variables

--- code/outils_stats.py
+++ code/outils_stats.py
@@ -3,7 +3,8 @@ Fonctions statistiques
 """
 
 import pandas as pd
-
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 def decrire_dataframe(df: pd.DataFrame) -> pd.DataFrame:
     """Retourner un dataframe avec des informations sur les colonnes d'un dataframe"""
@@ -19,3 +20,44 @@ def decrire_dataframe(df: pd.DataFrame) -> pd.DataFrame:
     return ret
 
 
+def list_colonnes_remplis(df: pd.DataFrame) -> list:
+    """Retourne un liste de colonnes 100% remplis"""
+    return df.columns[~df.isnull().any()].tolist()
+
+
+def plot_bar_remplissage(df: pd.DataFrame, nb=200, titre=None, figsize=(10, 5),
+                         vertical=False, filled_cols=False, thresh=50):
+    """Visualise pourcent remplissage des colonnes"""
+    remplissage = (round((1-df.isna().mean())*100)
+                   .sort_values(ascending=False)
+                   .to_frame(name='taux')
+                   .rename_axis('colonne').reset_index())
+    if filled_cols is False:
+        remplissage = remplissage[remplissage['taux'] < 99.99]
+    nb = min(nb, len(remplissage))
+    remplissage = remplissage.head(nb)
+    plt.figure(figsize=figsize)
+    remplissage['hue'] = remplissage['taux']//7 * 7
+    remplissage['hue'] = '>= ' + remplissage['hue'].astype(str)
+    fontsize = 14
+
+    if vertical:
+        ax = sns.barplot(data=remplissage, y='colonne',
+                         x='taux', hue='hue', dodge=False)
+        ax.set_xlabel('Pourcent remplissage (%)', size=fontsize)
+        ax.set_ylabel('Nom de la colonne', size=fontsize)
+        ax.axvline(thresh, linestyle='--', color='red')
+    else:
+        ax = sns.barplot(data=remplissage, x='colonne',
+                         y='taux', hue='hue', dodge=False)
+        ax.set_xlabel('Nom de la colonne', size=fontsize)
+        ax.set_ylabel('Pourcent remplissage (%)', size=fontsize)
+        plt.xticks(rotation=90)
+        ax.axhline(thresh, linestyle='--', color='red')
+    if titre is None:
+        titre = f'Taux de remplissage des {nb} colonnes plus remplis'
+    if not filled_cols:
+        titre = 'Taux de remplissage (sans colonnes 100% remplis)'
+    ax.set_title(label=f'{titre} [nb={df.shape[0]}]', size=fontsize+2)
+    plt.legend(title='remplissage (%)')
+    sns.despine()
