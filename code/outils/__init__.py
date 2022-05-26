a5dd1817befc399441cf7a5e50f4bf4ff0fb359b
mrcreasey <mrcreasey@gmail.com>
2022-05-26 12:49:16 +0200

    move library functions to notebook folder

--- code/outils/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Personal library
-"""
-
-from . import io, preprocess, stats, timed, vis
-
-__version__ = '0.0.1'
--- code/outils_stats.py
+++ code/outils_stats.py
@@ -5,10 +5,10 @@ Fonctions statistiques
 import pandas as pd
 
 
-
 def decrire_dataframe(df: pd.DataFrame) -> pd.DataFrame:
     """Retourner un dataframe avec des informations sur les colonnes d'un dataframe"""
-    print(f'{df.shape[0]} registres')
+    print(f'{df.shape[0]} registres', f'{df.shape[1]} colonnes')
+    print(f'dtypes={df.dtypes.value_counts()}')
     ret = df.columns.to_frame(name="column").set_index('column')
     ret['% manquantes'] = df.isna().mean()*100
     ret['unique'] = df.nunique()
