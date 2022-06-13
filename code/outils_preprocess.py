d426512a60cd6dce5ec25b2022210669bc64e68c
mrcreasey <mrcreasey@gmail.com>
2022-06-13 18:50:38 +0200

    improve reduce memory usage

--- code/outils_preprocess.py
+++ code/outils_preprocess.py
@@ -12,9 +12,7 @@ def reduce_memory(df: pd.DataFrame) -> pd.DataFrame:
     from : https://www.kaggle.com/code/jsaguiar/lightgbm-7th-place-solution/script
     """
     start_mem = df.memory_usage().sum() / 1024 ** 2
-    print('Initial df memory usage is {:.2f} MB for {} columns'
-          .format(start_mem, len(df.columns)))
-
+    print(f'Initial df memory usage is {start_mem:.2f} MB for {len(df.columns)} columns')
     for col in df.columns:
         col_type = df[col].dtypes
         if col_type != object:
@@ -40,8 +38,67 @@ def reduce_memory(df: pd.DataFrame) -> pd.DataFrame:
     end_mem = df.memory_usage().sum() / 1024 ** 2
     memory_reduction = 100 * (start_mem - end_mem) / start_mem
     print(
-        'Final memory usage is: {:.2f} MB - decreased by {:.1f}%'.format(end_mem, memory_reduction))
+        f'Final memory usage is: {end_mem:.2f} MB - decreased by {memory_reduction:.1f}%')
     return df
 
 
+def reduce_mem_usage(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    reduce memory usage of dataframe (improved) 
+    https://www.kaggle.com/code/arjanso/reducing-dataframe-memory-size-by-65/notebook
+    """
+   
+    start_mem = df.memory_usage().sum() / 1024 ** 2
+    print(f'Initial df memory usage is {start_mem:.2f} MB for {len(df.columns)} columns')
 
+    na_list = [] # Keeps track of columns that have missing values filled in. 
+    for col in df.columns:
+        if df[col].dtype != object:  # Exclude strings
+            
+            # make variables for Int, max and min
+            is_int = False
+            mx = df[col].max()
+            mn = df[col].min()
+            
+            # Integer does not support NA, therefore, NA needs to be filled
+            if not np.isfinite(df[col]).all(): 
+                na_list.append(col)
+                df[col].fillna(mn-1,inplace=True)  
+                   
+            # test if column can be converted to an integer
+            asint = df[col].fillna(0).astype(np.int64)
+            result = (df[col] - asint)
+            result = result.sum()
+            if result > -0.01 and result < 0.01:
+                is_int = True
+
+            
+            # Make Integer/unsigned Integer datatypes
+            if is_int:
+                if mn >= 0:
+                    if mx < 255:
+                        df[col] = df[col].astype(np.uint8)
+                    elif mx < 65535:
+                        df[col] = df[col].astype(np.uint16)
+                    elif mx < 4294967295:
+                        df[col] = df[col].astype(np.uint32)
+                    else:
+                        df[col] = df[col].astype(np.uint64)
+                else:
+                    if mn > np.iinfo(np.int8).min and mx < np.iinfo(np.int8).max:
+                        df[col] = df[col].astype(np.int8)
+                    elif mn > np.iinfo(np.int16).min and mx < np.iinfo(np.int16).max:
+                        df[col] = df[col].astype(np.int16)
+                    elif mn > np.iinfo(np.int32).min and mx < np.iinfo(np.int32).max:
+                        df[col] = df[col].astype(np.int32)
+                    elif mn > np.iinfo(np.int64).min and mx < np.iinfo(np.int64).max:
+                        df[col] = df[col].astype(np.int64)    
+            
+            # Make float datatypes 32 bit
+            else:
+                df[col] = df[col].astype(np.float32)
+    end_mem = df.memory_usage().sum() / 1024 ** 2
+    memory_reduction = 100 * (start_mem - end_mem) / start_mem
+    print(
+        f'Final memory usage is: {end_mem:.2f} MB - decreased by {memory_reduction:.1f}%')
+    return df
