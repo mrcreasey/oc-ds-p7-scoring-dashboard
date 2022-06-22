7c6bece12ffe7b699e6ba288c1782a9d242266be
mrcreasey <43987550+mrcreasey@users.noreply.github.com>
2022-06-22 21:24:45 +0200

    add route clients

--- api/app.py
+++ api/app.py
@@ -1,10 +1,24 @@
 from flask import Flask
+import pandas as pd
 
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/index/')
 def index():
-    return "Hello world !"
+    htmlstr= '<html><p>valid routes are :<p><ul>'
+    routes = ['clients', 'client/{id}']
+    for route in routes:
+        htmlstr+=f'<li>{route}</li>'
+    htmlstr+='</ul></html>'    
+    response = htmlstr
+    return response 
+
+@app.route('/clients/',  methods=['GET'])
+def clients():
+    # return (test) list of clients
+    response = pd.DataFrame([567,123,875], columns=['SK_ID_CURR']).to_json()
+    return response
 
 # python api/app.py -> runs locally on localhost port 5000
 if __name__ == "__main__":
